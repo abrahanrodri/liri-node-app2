d68816d7e16e91536dc2c2bd4d4d839bac110695
Abe <abrahan.rodri@gmail.com>
2019-07-25 12:00:19 -0400

    switch statement

--- liri.js
+++ liri.js
@@ -1,12 +1,26 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify1 = require('node-spotify-api');
+var spotify = require('node-spotify-api');
 var axios = require('axios');
 var moment = require('moment');
 var dotevn = require('dotenv');
 var fs = require('fs');
 
-var spotify2 = new Spotify(keys.spotify);
+var spotify = new Spotify(keys.spotify);
 
 var word1 = process.argv[2];
 var word2 = process.argv[3];
+
+
+switch(search) {
+    case "concert-this":
+      break;
+    case "spotify-this-song":
+      break;
+    case "movie-this":     
+      break;
+    case "do-what-it-says":     
+      break;
+    default:
+      text = "I have never heard of that fruit...";
+  }
