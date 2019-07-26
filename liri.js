1acaf7c286a56ecf034cec0cc41f0c57c3ad74e1
Abe <abrahan.rodri@gmail.com>
2019-07-25 23:52:33 -0400

    updating code

--- liri.js
+++ liri.js
@@ -9,18 +9,82 @@ var fs = require('fs');
 var spotify = new Spotify(keys.spotify);
 
 var word1 = process.argv[2];
-var word2 = process.argv[3];
+var word2 = process.argv.slice(3).join(" ");
 
 
-switch(search) {
+
+switch(word1) {
     case "concert-this":
+        searchBands();
       break;
     case "spotify-this-song":
+        searchSpotify();
       break;
-    case "movie-this":     
+    case "movie-this":  
+        searchOMDB();  
       break;
-    case "do-what-it-says":     
+    case "do-what-it-says": 
+        logDo();
       break;
     default:
-      text = "I have never heard of that fruit...";
+      console.log("I have never heard of that...");
   }
+
+
+  function searchBands(){
+    if (word2 === undefined) {
+        word2 = "drake";
+        searchBands(word2);
+    } else{
+        artistName = word2;
+        searchBands(word2);
+    }
+
+ var artistName = word2;
+ var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+
+ axios.get(queryUrl)
+ .then(function(response) {
+
+     var jsonData = response.data;
+             console.log("Venue: " + jsonData[i].venue.name);
+             console.log("Location: " + jsonData[i].venue.country + jsonData[i].venue.city);
+             console.log("Date: " + moment(jsonData[i].venue.datetime).format('MM/DD/YYYY'));
+         }
+        
+ )}
+
+  function searchSpotify(){
+
+  }
+
+  function searchOMDB(){
+    if (word2 === undefined) {
+        word2 = "the+avengers";
+        searchOMDB(word2);
+    } else{
+        movie = word2;
+        searchOMDB(word2);
+    } 
+    
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece")
+    .then(function (response) {
+        var jsonData = response.data;
+            console.log("Title of the movie: " + jsonData.title);
+            console.log("Year the movie came out: " + jsonData.Year);
+            console.log("IMDB Rating of the movie: " + jsonData.imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie: " + jsonData.ratings);
+            console.log("Country where the movie was produced: " + jsonData.Country);
+            console.log(" Language of the movie: " + jsonData.Language);
+            console.log("Plot of the movie: " + jsonData.Plot);
+            console.log("Actors in the movie: " + jsonData.Actors);
+        }
+    )
+  }
+
+  function logDo(){
+    fs.readFile('random.txt', 'Hello content!', function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      })
+    }
