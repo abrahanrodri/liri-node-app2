4f91cd04a0ebaecdd4d9e70a498eca9dbb37c16e
Abe <abrahan.rodri@gmail.com>
2019-07-26 00:05:49 -0400

    attempting spotify

--- liri.js
+++ liri.js
@@ -13,63 +13,77 @@ var word2 = process.argv.slice(3).join(" ");
 
 
 
-switch(word1) {
+switch (word1) {
     case "concert-this":
         searchBands();
-      break;
+        break;
     case "spotify-this-song":
         searchSpotify();
-      break;
-    case "movie-this":  
-        searchOMDB();  
-      break;
-    case "do-what-it-says": 
+        break;
+    case "movie-this":
+        searchOMDB();
+        break;
+    case "do-what-it-says":
         logDo();
-      break;
+        break;
     default:
-      console.log("I have never heard of that...");
-  }
+        console.log("I have never heard of that...");
+}
 
 
-  function searchBands(){
+function searchBands() {
     if (word2 === undefined) {
         word2 = "drake";
         searchBands(word2);
-    } else{
+    } else {
         artistName = word2;
         searchBands(word2);
     }
 
- var artistName = word2;
- var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
+    var artistName = word2;
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp";
 
- axios.get(queryUrl)
- .then(function(response) {
+    axios.get(queryUrl)
+        .then(function (response) {
 
-     var jsonData = response.data;
-             console.log("Venue: " + jsonData[i].venue.name);
-             console.log("Location: " + jsonData[i].venue.country + jsonData[i].venue.city);
-             console.log("Date: " + moment(jsonData[i].venue.datetime).format('MM/DD/YYYY'));
-         }
-        
- )}
+            var jsonData = response.data;
+            console.log("Name of the venue: " + jsonData[i].venue.name);
+            console.log("Venue location: " + jsonData[i].venue.country + jsonData[i].venue.city);
+            console.log("Date of the event: " + moment(jsonData[i].venue.datetime).format('MM/DD/YYYY'));
+        }
 
-  function searchSpotify(){
+        )
+}
 
-  }
+function searchSpotify() {
+    if (word2 === undefined) {
+        word2 = "madonna";
+        searchSpotify(word2);
+    } else {
+        artistName = word2;
+        searchSpotify(word2);
 
-  function searchOMDB(){
+        var jsonData=response.data;
+        console.log("Arist(s) on song: " + jsonData);
+        console.log("The song's name: " + jsonData);
+        console.log("A preview link of the song from Spotify: " + jsonData);
+        console.log("The album that the song is from: " + jsonData);
+    }
+}
+
+function searchOMDB() {
     if (word2 === undefined) {
         word2 = "the+avengers";
         searchOMDB(word2);
-    } else{
-        movie = word2;
+    } else {
+        movieName = word2;
         searchOMDB(word2);
-    } 
-    
-    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece")
-    .then(function (response) {
-        var jsonData = response.data;
+    }
+
+    axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece")
+        .then(function (response) {
+
+            var jsonData = response.data;
             console.log("Title of the movie: " + jsonData.title);
             console.log("Year the movie came out: " + jsonData.Year);
             console.log("IMDB Rating of the movie: " + jsonData.imdbRating);
@@ -79,12 +93,12 @@ switch(word1) {
             console.log("Plot of the movie: " + jsonData.Plot);
             console.log("Actors in the movie: " + jsonData.Actors);
         }
-    )
-  }
+        )
+}
 
-  function logDo(){
+function logDo() {
     fs.readFile('random.txt', 'Hello content!', function (err) {
         if (err) throw err;
         console.log('Saved!');
-      })
-    }
+    })
+}
